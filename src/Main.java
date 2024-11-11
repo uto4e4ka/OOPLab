fdb546ee8a658bc149b79cd97af46ca0104347fb
Uto4ka <alezhestkov@gmail.com>
2024-11-11 19:39:01 +0400

    11

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,13 @@ import vehicles.Automobile;
 import vehicles.Motorcycle;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Main {
     static final String PATH = "D:\\tgbot\\Lab3\\";
-    public static void main(String[] args) throws DuplicateModelNameException, NoSuchModelNameException, IOException, UnknownClassExeption, ClassNotFoundException, CloneNotSupportedException {
+    public static void main(String[] args) throws DuplicateModelNameException, NoSuchModelNameException, IOException, UnknownClassExeption, ClassNotFoundException, CloneNotSupportedException, NoSuchMethodException {
         Automobile automobile = new Automobile("BMW",4);
         automobile.addItem("Test",9999);
         Motorcycle motorcycle = new Motorcycle("Suzuki",10);
@@ -27,5 +30,12 @@ public class Main {
         Motorcycle m1 = motorcycle.clone();
         System.out.println(m1.toString());
         System.out.println(motorcycle);
+
+    }
+    void invokeMethod(String className) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+      Class<?> clazz = Class.forName(className);
+      Constructor<?> constructor = clazz.getConstructor(String.class,Integer.class);
+      Automobile automobile = (Automobile) constructor.newInstance("",2);
+      clazz.getMethod("",String.class,Integer.class).invoke(automobile,"",1);
     }
 }
