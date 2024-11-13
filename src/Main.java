3e05ee87e11a3fc8507289369268ab9e56ae0332
Alexey Zhestkov <alezhestkov@gmail.com>
2024-11-13 23:13:08 +0400

    Lab5

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import reflection.ReflectTools;
 import threads.PrintNamesThread;
 import threads.PrintPricesThread;
 import threads.runnable.PrintNamesRunnable;
+import threads.runnable.fileInput.FileReadModelsRunnable;
 import threads.runnable.synchronaized.SyncPrintNamesRunnable;
 import threads.runnable.synchronaized.SyncPrintPricesRunnable;
 import threads.runnable.synchronaized.PrintSynchronaizer;
@@ -16,64 +17,70 @@ import vehicles.*;
 
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
-    static final String PATH = "D:\\tgbot\\Lab3\\";
-    public static void main(String[] args) throws DuplicateModelNameException, NoSuchModelNameException, IOException, UnknownClassExeption, ClassNotFoundException, CloneNotSupportedException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-       Automobile automobile = new Automobile("BMW");
-       automobile.addItem("M1",34333);
-       automobile.addItem("M3",22323);
-       automobile.addItem("X6",114141);
-       automobile.addItem("X7",1231313);
-        PrintPricesThread prices = new PrintPricesThread(automobile);
-        PrintNamesThread names = new PrintNamesThread(automobile);
-        prices.setPriority(Thread.MAX_PRIORITY);
-        names.setPriority(Thread.MIN_PRIORITY);
-        prices.start();
-        names.start();
-        try {
-            prices.join();
-            names.join();
-        }catch (Exception e){
-
+    static final String PATH = "D:\\tgbot\\OOPLab\\";
+    public static void main(String[] args) throws DuplicateModelNameException, NoSuchModelNameException, IOException, UnknownClassExeption, ClassNotFoundException, CloneNotSupportedException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, InterruptedException {
+//       Automobile automobile = new Automobile("BMW");
+//       automobile.addItem("M1",34333);
+//       automobile.addItem("M3",22323);
+//       automobile.addItem("X6",114141);
+//       automobile.addItem("X7",1231313);
+//        PrintPricesThread prices = new PrintPricesThread(automobile);
+//        PrintNamesThread names = new PrintNamesThread(automobile);
+//        prices.setPriority(Thread.MAX_PRIORITY);
+//        names.setPriority(Thread.MIN_PRIORITY);
+//        prices.start();
+//        names.start();
+//        try {
+//            prices.join();
+//            names.join();
+//        }catch (Exception e){
+//
+//        }
+//        System.out.println("Вывод через синхронизатор");
+//        PrintSynchronaizer printSynchronaizer = new PrintSynchronaizer(automobile);
+//        Thread printPrice = new Thread(new SyncPrintNamesRunnable(printSynchronaizer));
+//        Thread printNames = new Thread(new SyncPrintPricesRunnable(printSynchronaizer));
+//        printPrice.start();
+//        printNames.start();
+//        try {
+//            printPrice.join();
+//            printNames.join();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println("Вывод через Reentrant");
+//        ReentrantLock reentrantLock = new ReentrantLock();
+//       Thread rPrintN = new Thread(new RePrintModelsRunnable(reentrantLock,automobile));
+//       Thread rPrintP = new Thread(new RePrintPricesRunnable(reentrantLock,automobile));
+//       rPrintN.start();
+//       rPrintP.start();
+//       try {
+//           rPrintN.join();
+//           rPrintP.join();
+//       } catch (InterruptedException e) {
+//           throw new RuntimeException(e);
+//       }
+//       System.out.println("С помощью ExecutorService");
+//        Scuter scuter = new Scuter("Scuter1",10);
+//       Motorcycle motorcycle = new Motorcycle("Moto",12);
+//       Moped moped = new Moped("Mop1",5);
+//        ExecutorService executors = Executors.newFixedThreadPool(2);
+//        executors.submit(new PrintNamesRunnable(automobile));
+//        executors.submit(new PrintNamesRunnable(scuter));
+//        executors.submit(new PrintNamesRunnable(motorcycle));
+//        executors.submit(new PrintNamesRunnable(moped));
+        String[] filePaths = {"ts1","ts2","ts3","ts4","ts5"};
+        BlockingQueue<Vehicle> blockingQueue = new ArrayBlockingQueue<>(5);
+        for(String s:filePaths){
+          new Thread(new FileReadModelsRunnable(PATH+s+".txt",blockingQueue)).start();
         }
-        System.out.println("Вывод через синхронизатор");
-        PrintSynchronaizer printSynchronaizer = new PrintSynchronaizer(automobile);
-        Thread printPrice = new Thread(new SyncPrintNamesRunnable(printSynchronaizer));
-        Thread printNames = new Thread(new SyncPrintPricesRunnable(printSynchronaizer));
-        printPrice.start();
-        printNames.start();
-        try {
-            printPrice.join();
-            printNames.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        for(int i =0;i<filePaths.length;i++){
+            System.out.println(blockingQueue.take().getBrand());
         }
-        System.out.println("Вывод через Reentrant");
-        ReentrantLock reentrantLock = new ReentrantLock();
-       Thread rPrintN = new Thread(new RePrintModelsRunnable(reentrantLock,automobile));
-       Thread rPrintP = new Thread(new RePrintPricesRunnable(reentrantLock,automobile));
-       rPrintN.start();
-       rPrintP.start();
-       try {
-           rPrintN.join();
-           rPrintP.join();
-       } catch (InterruptedException e) {
-           throw new RuntimeException(e);
-       }
-       System.out.println("С помощью ExecutorService");
-        Scuter scuter = new Scuter("Scuter1",10);
-       Motorcycle motorcycle = new Motorcycle("Moto",12);
-       Moped moped = new Moped("Mop1",5);
-        ExecutorService executors = Executors.newFixedThreadPool(2);
-        executors.submit(new PrintNamesRunnable(automobile));
-        executors.submit(new PrintNamesRunnable(scuter));
-        executors.submit(new PrintNamesRunnable(motorcycle));
-        executors.submit(new PrintNamesRunnable(moped));
-        String[] filePaths = {""}
     }
    static void getModel(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         String name = args[0];
--- src/threads/runnable/fileInput/FileReadModelsRunnable.java
+++ src/threads/runnable/fileInput/FileReadModelsRunnable.java
@@ -7,13 +7,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
 
 public class FileReadModelsRunnable implements Runnable{
 String path;
-BlockingDeque<Vehicle> blockingDeque;
-public FileReadModelsRunnable(String path,BlockingDeque<Vehicle> blockingDeque){
+BlockingQueue<Vehicle> blockingQueue;
+public FileReadModelsRunnable(String path, BlockingQueue<Vehicle> blockingQueue){
     this.path = path;
-    this.blockingDeque = blockingDeque;
+    this.blockingQueue = blockingQueue;
 }
     @Override
     public void run() {
@@ -21,7 +22,8 @@ public FileReadModelsRunnable(String path,BlockingDeque<Vehicle> blockingDeque){
             Scanner scanner = new Scanner(new FileReader(path));
             String modelName = scanner.nextLine();
             Vehicle vehicle = new Automobile(modelName);
-            blockingDeque.put(vehicle);
+            blockingQueue.put(vehicle);
+           // System.out.println("Считано: "+modelName);
         } catch (FileNotFoundException | InterruptedException e) {
             throw new RuntimeException(e);
         }
